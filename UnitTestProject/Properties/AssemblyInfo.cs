de28b11a4faccc269e52fa7cf2271b38693294e3
GoodestUsername <59536503+GoodestUsername@users.noreply.github.com>
2021-04-12 23:34:11 -0700

    added unit tests start

---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("015fac59-c75c-4158-afba-3a0099ef29cb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject/StudentUnitTest.cs
@@ -0,0 +1,63 @@
+﻿using BCITDesktop;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTestStudent
+    {
+
+        /**
+        * Firebase initialization
+        */
+        IFirebaseClient client;
+        IFirebaseConfig firebaseConfigurations = new FirebaseConfig()
+        {
+            AuthSecret = "xyEfrWdHzVWmoXvV11MFgTmMRv8g28oLaJs8kRnH",
+            BasePath = "https://bcitdesktop-default-rtdb.firebaseio.com/"
+        };
+
+        [TestMethod]
+        public void TestMethodGetStudent()
+        {
+            //Arrange
+            client = new FireSharp.FirebaseClient(firebaseConfigurations);
+            if (client == null)
+            {
+                Console.WriteLine("Test failed, cannot connect to firebase");
+                return;
+            }
+            String testStudentID = "A00000001";
+            Student actualStudent;
+
+            // expected student values for test student in database
+            Student expectedStudent = new Student()
+            {
+                FirstName = "TestFirstName",
+                LastName = "TestLastName",
+                StudentNumber = testStudentID,
+                Email = "teststudent@test.test",
+                Password = "TestPassword",
+                Gender = "Male",
+                Phone = "0000000000",
+                DateOfBirth = DateTime.Parse("2021-04-12T00:00:00-07:00")
+            };
+
+            //Act
+            actualStudent = Student.getStudent(client, testStudentID);
+
+            //Assert
+            Assert.AreEqual(expectedStudent.FirstName, actualStudent.FirstName);
+            Assert.AreEqual(expectedStudent.LastName, actualStudent.LastName);
+            Assert.AreEqual(expectedStudent.StudentNumber, actualStudent.StudentNumber);
+            Assert.AreEqual(expectedStudent.Email, actualStudent.Email);
+            Assert.AreEqual(expectedStudent.Password, actualStudent.Password);
+            Assert.AreEqual(expectedStudent.Gender, actualStudent.Gender);
+            Assert.AreEqual(expectedStudent.Phone, actualStudent.Phone);
+            Assert.AreEqual(expectedStudent.DateOfBirth, actualStudent.DateOfBirth);
+        }
+    }
+}
